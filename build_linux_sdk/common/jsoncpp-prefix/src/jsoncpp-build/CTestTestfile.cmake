e624eefd2f87c56e4340c35a834ebd14b96bb19c
kaifenghyw <86344862+kaifenghyw@users.noreply.github.com>
2021-09-27 16:05:26 +0800

    Delete temporary files

--- build_linux_sdk/common/jsoncpp-prefix/src/jsoncpp-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guogang.zhb/github/alibabacloud-nls-cpp-sdk/build_linux_sdk/common/jsoncpp-prefix/src/jsoncpp
-# Build directory: /home/guogang.zhb/github/alibabacloud-nls-cpp-sdk/build_linux_sdk/common/jsoncpp-prefix/src/jsoncpp-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("include")
--- build_linux_sdk/common/jsoncpp-prefix/src/jsoncpp-build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guogang.zhb/github/alibabacloud-nls-cpp-sdk/build_linux_sdk/common/jsoncpp-prefix/src/jsoncpp/src
-# Build directory: /home/guogang.zhb/github/alibabacloud-nls-cpp-sdk/build_linux_sdk/common/jsoncpp-prefix/src/jsoncpp-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib_json")
